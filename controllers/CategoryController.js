d1f6919c364576f224b098c35e952eda7206c004
Asif Salman Malik <35447876+asifmalik106@users.noreply.github.com>
2025-01-18 11:05:00 +0900

    [fix] get productCategory by categoryID

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -60,7 +60,7 @@ module.exports = class CategoryController {
         if(error=="Wrong Category ID"){
           return Response.conflict(res, "Category Does Not Exists!", error);
         }
-        console.log(error)
+        console.log(error.Count)
         console.log(typeof error)
         Response.badRequest(res,"Something Went Wrong!", JSON.stringify(error))
       }
